d1791344d15910f05c8e691755a10704717f5dfb
Kevin Bieri <bierike@gmail.com>
2021-09-19 22:40:10 +0200

    remove unnecessary head tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,6 @@ export default {
   head: {
     titleTemplate: '%s | Maestro',
     title: '…',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Manage customers, tasks and invoices.' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   css: ['~/assets/main.css'],
   plugins: [
